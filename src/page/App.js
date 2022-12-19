b0603be86f02d14840ed45505526dff1546236bf
Abdulaziz <aoqilov88@gmail.com>
2022-12-20 00:20:55 +0500

    start

---
+++ src/page/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+//
+import Chat from "./Chat";
+import Future from "./Future";
+import Home from "./Home";
+import Month from "./Month";
+import Staff from "./Staff";
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+      <Staff />
+      <Month />
+      <Chat />
+      <Future />
+    </div>
+  );
+};
+
+export default App;
